43d7578b49740a27474a17674babc4f28b5d0d39
yyr0204 <yyr0204@naver.com>
2023-11-02 15:45:52 +0900

    manager nim feedback

--- src/main/java/com/dogsole/developersite/account/entity/vender/VenderEntity.java
+++ src/main/java/com/dogsole/developersite/account/entity/vender/VenderEntity.java
@@ -35,7 +35,6 @@ public class VenderEntity {
     private String venderPasswd;
 
     @Column(name = "create_dt")
-    @CreationTimestamp
     private LocalDateTime createDt =LocalDateTime.now();
 
     @Column(name = "update_dt")
